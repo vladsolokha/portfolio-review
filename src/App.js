cf05408b01478f6953fb4f1f42458d28d0aa2f92
vladsolokha <vsolokha@gmail.com>
2021-07-10 00:04:39 -0600

    made 2 of everything appear. 2 classes of random names.

--- src/App.js
+++ src/App.js
@@ -2,36 +2,52 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 const students = ['joe ', 'billy ', 'bob ', 'jane ', 'john ', 'isaac ', 'vlad ']; //will need to be imported from file or so
+const students2 = ['gail', 'jaden', 'kai', 'bobby', 'a-a', 'fierce', 'eden', 'tanya']
 
 function App() {
 
   const [names, setNames] = useState(students); //can store objects as state, can call in objects from outside of block
+  const [names2, setNames2] = useState(students2); 
+  const [randomName, setRandomName] = useState(''); //sets state to get random name
+  const [randomName2, setRandomName2] = useState('');
+
   const min = 0;
   const max = names.length;
-  
-  const [randomName, setRandomName] = useState(''); //sets state to get random name
-  
-  useEffect(() => {
-    buttonHandler();
-  })
-  
+  const max2 = names2.length;
+
   const buttonHandler = () => {
     const randindex = Math.floor(Math.random() * (max - min) + min);
     const getName = names[randindex];
-
     setRandomName(getName)
   }
+  const buttonHandler2 = () => {
+    const randindex2 = Math.floor(Math.random() * (max2 - min) + min);
+    const getName = names2[randindex2];
+    setRandomName2(getName)
+  }
+
 
   return (
     <div className="App">
       <header className="App-header">
-        
-        <p>
-          {randomName} {/*called from state variable above*/}
-        </p>
-                
-        <button onClick={buttonHandler}>Get Name</button>
-      
+        <div className="section1">
+          <p>
+            {randomName} {/*called from state variable above*/}
+          </p>
+                  
+          <button onClick={buttonHandler}>
+            Get A Random Name Class 1
+          </button>
+        </div>
+        <div className="section2">
+          <p>
+            {randomName2}
+          </p> 
+
+          <button onClick={buttonHandler2}>
+            Get A Random Name Class 2
+          </button>
+        </div>
       </header>
     </div>
   );
